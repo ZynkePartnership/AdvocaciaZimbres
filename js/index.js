104849df03d74a1cf147f931c56a7bb9b6058933
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-05-18 13:14:39 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const navbarToggle = document.querySelector(".nav-content-btn");
 const navbarCollapse = document.querySelector("nav");
+const iconBarAnim = document.querySelector("icon-bar");
 
 navbarToggle.addEventListener("click", showBtn);
 
 function showBtn(){
     navbarCollapse.classList.toggle("show");
+    iconBarAnim.classList.toggle("show");
 }
